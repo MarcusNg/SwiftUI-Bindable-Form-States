20b37db0cabec8cbd7115d56ffbd7c72a27a19a7
Marcus Ng <marcuslistanng@gmail.com>
2019-06-29 15:47:53 -0400

    Initial Commit

---
+++ SwiftUI-Bindable-Form-States/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Bindable-Form-States
+//
+//  Created by Marcus Ng on 6/29/19.
+//  Copyright © 2019 Amarok Technologies Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
